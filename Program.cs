30c20a60f4239e0cb2b21ace52d52411575602ec
kundan <goswamikundan123@gmail.com>
2023-02-11 19:01:15 +0530

    Adding Use Case one file which display Initial position of Player One

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to snake and ladder Game");
+            SnakeladderUseCaseOne.DisplayInitialPos();
         }
     }
 }
---
+++ SnakeladderUseCaseOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class SnakeladderUseCaseOne
+    {
+        public static void DisplayInitialPos()
+        {
+            Console.WriteLine("This is start of SnakeLadder Game.");
+            int position = 0;
+            Console.WriteLine("Initial Position of Player: " + position);
+
+        }
+    }
+}
